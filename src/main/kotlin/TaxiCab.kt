427f1bf922a9475bdd1c7f560adf9d19a9cfa3d3
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2017-01-17 20:00:55 +0000

    Refactoring
    Signed-off-by: Justin Wiblin <justin_wiblin@hotmail.co.uk>

---
+++ src/main/kotlin/TaxiCab.kt
@@ -0,0 +1,60 @@
+import java.math.BigInteger
+import java.util.*
+
+class TaxiCab(val Entry: Map.Entry<BigInteger, List<List<BigInteger>>>,val Power : Int) {
+
+    override fun toString(): String {
+        return "${Entry.key} = ${Entry.value.map { it.joinToString("^$Power + ", "", "^$Power") }.joinToString(" = ")}"
+    }
+    val key = Entry.key
+    val value = Entry.value
+    companion object{
+        operator fun invoke(power: Int, amount: Int, ways: Int): TaxiCab? {
+            return taxiCabImpl(power, amount, ways, Byte.MAX_VALUE.toLong())
+        }
+
+        tailrec private fun taxiCabImpl(power: Int, amount: Int, ways: Int, max: Long): TaxiCab? {
+            val results = getSubsets((1L..max).map {
+                BigInteger.valueOf(it)
+            }.toList(), amount).map {
+                it.map { it.pow(power) }.fold(BigInteger.ZERO) { a, b -> a + b } to it
+            }.groupBy { it.first }.map { it.key to it.value.map { it.second }.toList() }.toMap()
+            val smallest = results.filter { it.value.size >= ways }.minBy { it.key }
+            if (smallest != null) {
+                return TaxiCab(smallest,power)
+            } else if (max < Long.MAX_VALUE) {
+                (results as LinkedHashMap).clear()
+                return taxiCabImpl(power, amount, ways, if (max * 2 > 0) max * 2 else Long.MAX_VALUE)
+            } else {
+                return null
+            }
+
+
+        }
+
+        tailrec private fun <T> getSubsets(superSet: List<T>, k: Int, idx: Int, current: ArrayList<T>, solution: ArrayList<List<T>>) {
+
+            //successful stop clause
+            if (current.size == k) {
+                solution.add(ArrayList(current))
+                return
+            }
+            //unsuccessful stop clause
+            if (idx == superSet.size) return
+            val x = superSet[idx]
+            current.add(x)
+            //"guess" x is in the subset
+            getSubsets(superSet, k, idx + 1, current, solution)
+            current.remove(x)
+            //"guess" x is not in the subset
+            getSubsets(superSet, k, idx + 1, current, solution)
+        }
+
+        private fun <T> getSubsets(superSet: List<T>, k: Int): List<List<T>> {
+            val res = ArrayList<List<T>>()
+            getSubsets(superSet, k, 0, ArrayList<T>(), res)
+            res.addAll(superSet.map { a -> (1..k).map { a }.toList() })
+            return res
+        }
+    }
+}
--- src/test/kotlin/TaxiCabTest.kt
+++ src/test/kotlin/TaxiCabTest.kt
@@ -4,62 +4,62 @@ import kotlin.test.assertEquals
 
 class TaxiCabTest {
     @Test fun TaxiCab122() {
-        val result = TaxiCab.apply(1, 2, 2)
+        val result = TaxiCab(1, 2, 2)
         assertEquals(BigInteger.valueOf(4L),result?.key)
 
     }
     @Test fun TaxiCab222() {
-        val result = TaxiCab.apply(2, 2, 2)
+        val result = TaxiCab(2, 2, 2)
         assertEquals(BigInteger.valueOf(50L),result?.key)
 
     }
     @Test fun TaxiCab322() {
-        val result = TaxiCab.apply(3, 2, 2)
+        val result = TaxiCab(3, 2, 2)
         assertEquals(BigInteger.valueOf(1729L),result?.key)
 
     }
     @Test fun TaxiCab422() {
-        val result = TaxiCab.apply(4, 2, 2)
+        val result = TaxiCab(4, 2, 2)
         assertEquals(BigInteger.valueOf(635318657L),result?.key)
 
     }
     @Test fun TaxiCab133() {
-        val result = TaxiCab.apply(1, 3, 3)
+        val result = TaxiCab(1, 3, 3)
         assertEquals(BigInteger.valueOf(9L),result?.key)
 
     }
     @Test fun TaxiCab233() {
-        val result = TaxiCab.apply(2, 3, 3)
+        val result = TaxiCab(2, 3, 3)
         assertEquals(BigInteger.valueOf(101L),result?.key)
 
     }
     @Test fun TaxiCab333() {
-        val result = TaxiCab.apply(3, 3, 3)
+        val result = TaxiCab(3, 3, 3)
         assertEquals(BigInteger.valueOf(5104L),result?.key)
 
     }
     @Test fun TaxiCab433() {
-        val result = TaxiCab.apply(4, 3, 3)
+        val result = TaxiCab(4, 3, 3)
         assertEquals(BigInteger.valueOf(811538L),result?.key)
 
     }
     @Test fun TaxiCab144() {
-        val result = TaxiCab.apply(1, 4, 4)
+        val result = TaxiCab(1, 4, 4)
         assertEquals(BigInteger.valueOf(14L),result?.key)
 
     }
     @Test fun TaxiCab244() {
-        val result = TaxiCab.apply(2, 4, 4)
+        val result = TaxiCab(2, 4, 4)
         assertEquals(BigInteger.valueOf(126L),result?.key)
 
     }
     @Test fun TaxiCab344() {
-        val result = TaxiCab.apply(3, 4, 4)
+        val result = TaxiCab(3, 4, 4)
         assertEquals(BigInteger.valueOf(4445L),result?.key)
 
     }
     @Test fun TaxiCab444() {
-        val result = TaxiCab.apply(4, 4, 4)
+        val result = TaxiCab(4, 4, 4)
         assertEquals(BigInteger.valueOf(300834L),result?.key)
 
     }
