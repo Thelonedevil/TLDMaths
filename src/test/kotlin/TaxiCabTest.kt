73ee931b128a5cd7a57ad4790544974d9b5729c0
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2017-01-18 10:17:52 +0000

    Restructured tests, removed some that used too much memory for travis to handle, coverage should still be the same
    Signed-off-by: Justin Wiblin <justin_wiblin@hotmail.co.uk>

--- src/test/kotlin/TaxiCabTest.kt
+++ src/test/kotlin/TaxiCabTest.kt
@@ -1,66 +1,88 @@
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import java.math.BigInteger
 import kotlin.test.assertEquals
 
+
 class TaxiCabTest {
+
+    @Rule @JvmField
+    var name = TestName()
+
+    private var start: Long = 0
+    private var result: TaxiCab? = null
+
+    @Before
+    fun start() {
+        start = System.currentTimeMillis()
+        print(name.methodName)
+    }
+
+    @After
+    fun end() {
+        println(" = ${result?.key},took ${System.currentTimeMillis() - start} ms")
+
+    }
+
     @Test fun TaxiCab122() {
-        val result = TaxiCab(1, 2, 2)
-        assertEquals(BigInteger.valueOf(4L),result?.key)
+        result = TaxiCab(1, 2, 2)
+        assertEquals(BigInteger.valueOf(4L), result?.key)
 
     }
+
     @Test fun TaxiCab222() {
-        val result = TaxiCab(2, 2, 2)
-        assertEquals(BigInteger.valueOf(50L),result?.key)
+        result = TaxiCab(2, 2, 2)
+        assertEquals(BigInteger.valueOf(50L), result?.key)
 
     }
+
     @Test fun TaxiCab322() {
-        val result = TaxiCab(3, 2, 2)
-        assertEquals(BigInteger.valueOf(1729L),result?.key)
-
+        result = TaxiCab(3, 2, 2)
+        assertEquals(BigInteger.valueOf(1729L), result?.key)
     }
+
     @Test fun TaxiCab422() {
-        val result = TaxiCab(4, 2, 2)
-        assertEquals(BigInteger.valueOf(635318657L),result?.key)
-
+        result = TaxiCab(4, 2, 2)
+        assertEquals(BigInteger.valueOf(635318657L), result?.key)
     }
+
     @Test fun TaxiCab133() {
-        val result = TaxiCab(1, 3, 3)
-        assertEquals(BigInteger.valueOf(9L),result?.key)
+        result = TaxiCab(1, 3, 3)
+        assertEquals(BigInteger.valueOf(9L), result?.key)
 
     }
+
     @Test fun TaxiCab233() {
-        val result = TaxiCab(2, 3, 3)
-        assertEquals(BigInteger.valueOf(101L),result?.key)
+        result = TaxiCab(2, 3, 3)
+        assertEquals(BigInteger.valueOf(101L), result?.key)
 
     }
+
     @Test fun TaxiCab333() {
-        val result = TaxiCab(3, 3, 3)
-        assertEquals(BigInteger.valueOf(5104L),result?.key)
+        result = TaxiCab(3, 3, 3)
+        assertEquals(BigInteger.valueOf(5104L), result?.key)
 
     }
+
     @Test fun TaxiCab433() {
-        val result = TaxiCab(4, 3, 3)
-        assertEquals(BigInteger.valueOf(811538L),result?.key)
+        result = TaxiCab(4, 3, 3)
+        assertEquals(BigInteger.valueOf(811538L), result?.key)
 
     }
-    @Test fun TaxiCab144() {
-        val result = TaxiCab(1, 4, 4)
-        assertEquals(BigInteger.valueOf(14L),result?.key)
+
+    @Test fun TaxiCab321() {
+        result = TaxiCab(3, 2, 1)
+        assertEquals(BigInteger.valueOf(2L), result?.key)
 
     }
-    @Test fun TaxiCab244() {
-        val result = TaxiCab(2, 4, 4)
-        assertEquals(BigInteger.valueOf(126L),result?.key)
+
+    @Test fun TaxiCab323() {
+        result = TaxiCab(3, 2, 3)
+        assertEquals(BigInteger.valueOf(87539319L), result?.key)
 
     }
-    @Test fun TaxiCab344() {
-        val result = TaxiCab(3, 4, 4)
-        assertEquals(BigInteger.valueOf(4445L),result?.key)
 
-    }
-    @Test fun TaxiCab444() {
-        val result = TaxiCab(4, 4, 4)
-        assertEquals(BigInteger.valueOf(300834L),result?.key)
-
-    }
 }
